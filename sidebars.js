148e1b45ddb6ef048ae0a14701a4b3cd25d7df90
solufa <solufa2020@gmail.com>
2020-11-04 05:01:10 +0900

    feat: add deployment

--- sidebars.js
+++ sidebars.js
@@ -1,13 +1,16 @@
 module.exports = {
   someSidebar: [
     'introduction',
+    { Installation: ['installation/gui', 'installation/cui'] },
+    'entrypoint',
     {
-      Installation: ['installation/gui', 'installation/cui'],
-      Entrypoint: ['entrypoint/server'],
       Controller: ['controller/get-tasks', 'controller/get-task-by-id', 'controller/post-tasks', 'controller/posting-formdata'],
       Hooks: ['hooks/lifecycle', 'hooks/directory-level-hooks', 'hooks/controller-level-hooks', 'hooks/additional-request'],
       Validation: ['validation/path-parameter', 'validation/url-query', 'validation/json-body', 'validation/custom-validation'],
       'O/R mapping tool': ['orm/prisma', 'orm/typeorm'],
+    },
+    'deployment',
+    {
       Advanced: ['advanced/error-handling', 'advanced/cors-helmet', 'advanced/performance']
     }
   ],
