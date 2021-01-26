e34d83f31b6ea214036efcd659509685e3ce66b9
subaru <s@su8ru.dev>
2021-01-26 13:29:56 +0900

    fix: lint:fix

--- src/pages/changelog.tsx
+++ src/pages/changelog.tsx
@@ -1,31 +1,31 @@
-import React, { useState, useEffect } from 'react';
-import clsx from 'clsx';
-import Layout from '@theme/Layout';
-import Tabs from '@theme/Tabs';
-import TabItem from '@theme/TabItem';
-import Link from '@docusaurus/Link';
-import styles from './changelog.module.css';
+import React, { useState, useEffect } from 'react'
+import clsx from 'clsx'
+import Layout from '@theme/Layout'
+import Tabs from '@theme/Tabs'
+import TabItem from '@theme/TabItem'
+import Link from '@docusaurus/Link'
+import styles from './changelog.module.css'
 import axios from 'axios'
 import ReactMarkdown from 'react-markdown'
 import moment from 'moment'
 
 interface Release {
-  url: string;
-  html_url: string;
-  id: string;
-  node_id: string;
-  tag_name: string;
-  target_commitish: string;
-  name: string;
-  body: string;
-  draft: boolean;
-  prerelease: boolean;
-  created_at: string;
-  published_at: string;
+  url: string
+  html_url: string
+  id: string
+  node_id: string
+  tag_name: string
+  target_commitish: string
+  name: string
+  body: string
+  draft: boolean
+  prerelease: boolean
+  created_at: string
+  published_at: string
 }
 
 interface ReleaseProps {
-  release: Release;
+  release: Release
 }
 
 const Release: React.FC<ReleaseProps> = (props) => {
@@ -33,34 +33,38 @@ const Release: React.FC<ReleaseProps> = (props) => {
     <article className={styles.release}>
       <div className={styles.leftBox}>
         <h2 className={styles.version}>
-          <Link href={props.release.html_url}>
-            {props.release.name}
-          </Link>
+          <Link href={props.release.html_url}>{props.release.name}</Link>
         </h2>
         <span className={styles.date}>
-          {moment(props.release.created_at).format("YYYY.MM.DD")}
+          {moment(props.release.created_at).format('YYYY.MM.DD')}
         </span>
       </div>
-      <ReactMarkdown className={styles.rightBox} children={props.release.body} />
+      <ReactMarkdown className={styles.rightBox}>
+        {props.release.body}
+      </ReactMarkdown>
     </article>
   )
 }
 
 const Changelog = () => {
-  const [ frourioReleases, setFrourioReleases ] = useState<Release[]>([])
-  const [ cfaReleases, setCfaReleases ] = useState<Release[]>([])
+  const [frourioReleases, setFrourioReleases] = useState<Release[]>([])
+  const [cfaReleases, setCfaReleases] = useState<Release[]>([])
 
   useEffect(() => {
     const _f = async () => {
-      const res = await axios.get<Release[]>("https://api.github.com/repos/frouriojs/frourio/releases?per_page=100");
+      const res = await axios.get<Release[]>(
+        'https://api.github.com/repos/frouriojs/frourio/releases?per_page=100'
+      )
       setFrourioReleases(res.data)
     }
     _f()
   }, [setFrourioReleases])
-  
+
   useEffect(() => {
     const _f = async () => {
-      const res = await axios.get<Release[]>("https://api.github.com/repos/frouriojs/create-frourio-app/releases?per_page=100");
+      const res = await axios.get<Release[]>(
+        'https://api.github.com/repos/frouriojs/create-frourio-app/releases?per_page=100'
+      )
       setCfaReleases(res.data)
     }
     _f()
@@ -71,36 +75,36 @@ const Changelog = () => {
       title="Changelog"
       description="Changelog of frourio and create-frourio-app"
     >
-      <main className={clsx("container", styles.main)}>
+      <main className={clsx('container', styles.main)}>
         <h1 className={styles.title}>Changelog</h1>
-        <p>
-          Release list of frourio and create-frourio-app.
-        </p>
-        <span>
-          See all releases on GitHub:
-        </span>
+        <p>Release list of frourio and create-frourio-app.</p>
+        <span>See all releases on GitHub:</span>
         <ul>
           <li>
-            <Link href="https://github.com/frouriojs/frourio/releases">frouriojs/frourio</Link>
+            <Link href="https://github.com/frouriojs/frourio/releases">
+              frouriojs/frourio
+            </Link>
           </li>
           <li>
-            <Link href="https://github.com/frouriojs/create-frourio-app/releases">frouriojs/create-frourio-app</Link>
+            <Link href="https://github.com/frouriojs/create-frourio-app/releases">
+              frouriojs/create-frourio-app
+            </Link>
           </li>
         </ul>
         <Tabs
           defaultValue="frourio"
           values={[
             { label: 'frourio', value: 'frourio' },
-            { label: 'create-frourio-app', value: 'create-frourio-app' },
-          ]
-        }>
+            { label: 'create-frourio-app', value: 'create-frourio-app' }
+          ]}
+        >
           <TabItem value="frourio">
-            {frourioReleases.map(item => (
+            {frourioReleases.map((item) => (
               <Release release={item} key={item.id} />
             ))}
           </TabItem>
           <TabItem value="create-frourio-app">
-            {cfaReleases.map(item => (
+            {cfaReleases.map((item) => (
               <Release release={item} key={item.id} />
             ))}
           </TabItem>
