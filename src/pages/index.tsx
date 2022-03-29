c8ce8bf4a7e035dc6898caac87f2a15aa1083c84
subaru <s@su8ru.dev>
2022-03-30 00:30:15 +0900

    feat: add link to tutorial in top page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -140,6 +140,12 @@ function Home() {
             >
               Get Started
             </Link>
+            <Link
+              className={clsx('button button--outline button--primary button--lg', styles.getStarted)}
+              to={useBaseUrl('/docs/tutorial/introduction/')}
+            >
+              Start Tutorial
+            </Link>
           </div>
           <div className="margin-top--md">
             <GitHubButton
