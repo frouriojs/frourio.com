97d5c12684acc8681df59097ed9c28a6e13c1a88
subaru <main@su8ru.dev>
2022-09-27 14:39:48 +0900

    feat: add links to subdomains (not officially supported)

--- src/theme/NavbarItem/LocaleDropdownNavbarItem/index.js
+++ src/theme/NavbarItem/LocaleDropdownNavbarItem/index.js
@@ -1,51 +1,35 @@
 import React from 'react';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import { useAlternatePageUtils } from '@docusaurus/theme-common/internal';
-import { translate } from '@docusaurus/Translate';
 import DropdownNavbarItem from '@theme/NavbarItem/DropdownNavbarItem';
 import IconLanguage from '@theme/Icon/Language';
 import styles from './styles.module.css';
+import { useLocation } from 'react-router-dom';
+// ==== 2022.09.27 | Modified to add links to subdomains (not officially supported) ====
 export default function LocaleDropdownNavbarItem({
   mobile,
   dropdownItemsBefore,
   dropdownItemsAfter,
   ...props
 }) {
-  const {
-    i18n: { currentLocale, locales, localeConfigs },
-  } = useDocusaurusContext();
-  const alternatePageUtils = useAlternatePageUtils();
-  const localeItems = locales.map((locale) => {
-    const to = `pathname://${alternatePageUtils.createUrl({
-      locale,
-      fullyQualified: false,
-    })}`;
-    return {
-      label: localeConfigs[locale].label,
-      to,
+  const location = useLocation();
+  const localeItems = [
+    {
+      label: 'English',
+      to: 'https://frourio.com' + location.pathname,
       target: '_self',
       autoAddBaseUrl: false,
-      className:
-        // eslint-disable-next-line no-nested-ternary
-        locale === currentLocale
-          ? // Similar idea as DefaultNavbarItem: select the right Infima active
-            // class name. This cannot be substituted with isActive, because the
-            // target URLs contain `pathname://` and therefore are not NavLinks!
-            mobile
-            ? 'menu__link--active'
-            : 'dropdown__link--active'
-          : '',
-    };
-  });
+      className: mobile ? 'menu__link--active' : 'dropdown__link--active',
+    },
+    {
+      label: '日本語',
+      to: 'https://ja.frourio.com' + location.pathname,
+      target: '_self',
+      autoAddBaseUrl: false,
+      className: '',
+    },
+  ];
   const items = [...dropdownItemsBefore, ...localeItems, ...dropdownItemsAfter];
   // Mobile is handled a bit differently
-  const dropdownLabel = mobile
-    ? translate({
-        message: 'Languages',
-        id: 'theme.navbar.mobileLanguageDropdown.label',
-        description: 'The label for the mobile language switcher dropdown',
-      })
-    : localeConfigs[currentLocale].label;
+  const dropdownLabel = mobile ? 'Languages' : 'English';
   return (
     <DropdownNavbarItem
       {...props}
