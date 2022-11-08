ab646b40de1a4a684108dc67004b868ba427ec0c
subaru <main@su8ru.dev>
2022-11-08 18:59:50 +0900

    feat: replace type dev icon with svg

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -191,14 +191,32 @@ function Home() {
           <div className={clsx('text--center', styles.feature)}>
             <div className="container">
               <div className={styles.typeDevDescription}>
-                <img className={styles.typeDevDescriptionIcon} src="img/communication.jpg" />
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  className={styles.typeDevDescriptionIcon}
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M1 11.5a.5.5 0 0 0 .5.5h11.793l-3.147 3.146a.5.5 0 0 0 .708.708l4-4a.5.5 0 0 0 0-.708l-4-4a.5.5 0 0 0-.708.708L13.293 11H1.5a.5.5 0 0 0-.5.5zm14-7a.5.5 0 0 1-.5.5H2.707l3.147 3.146a.5.5 0 1 1-.708.708l-4-4a.5.5 0 0 1 0-.708l4-4a.5.5 0 1 1 .708.708L2.707 4H14.5a.5.5 0 0 1 .5.5z"
+                  />
+                </svg>
                 <p className={styles.highlightTextBlk}>
                   Since API type definition forces the controller type and http request, the test
                   code is not necessary for communication.
                 </p>
               </div>
               <div className={styles.typeDevDescription}>
-                <img className={styles.typeDevDescriptionIcon} src="img/speed.png" />
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  className={styles.typeDevDescriptionIcon}
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M8.5 5.6a.5.5 0 1 0-1 0v2.9h-3a.5.5 0 0 0 0 1H8a.5.5 0 0 0 .5-.5V5.6z" />
+                  <path d="M6.5 1A.5.5 0 0 1 7 .5h2a.5.5 0 0 1 0 1v.57c1.36.196 2.594.78 3.584 1.64a.715.715 0 0 1 .012-.013l.354-.354-.354-.353a.5.5 0 0 1 .707-.708l1.414 1.415a.5.5 0 1 1-.707.707l-.353-.354-.354.354a.512.512 0 0 1-.013.012A7 7 0 1 1 7 2.071V1.5a.5.5 0 0 1-.5-.5zM8 3a6 6 0 1 0 .001 12A6 6 0 0 0 8 3z" />
+                </svg>
                 <p className={styles.highlightTextBlk}>
                   Setting up a full-stack development environment is simple, so the product can be
                   completed quickly and safely.
